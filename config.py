1dc26a8c90197a2be26fb93ae3b71848390938ca
markdesanta9-prog <markdesanta9@gmail.com>
2026-06-18 12:04:49 +0300

    Create config.py
    import os
    
    BOT_TOKEN = os.getenv("BOT_TOKEN", "8481002138:AAHtukBqtj9B_IqsQDulkeKtiaSSKnOU8-c")
    CHANNEL_ID = os.getenv("CHANNEL_ID", "@mariushHP2009")
    
    OWM_API_KEY = os.getenv("OWM_API_KEY", "")
    NEWS_API_KEY = os.getenv("NEWS_API_KEY", "")
    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
    
    CITY = "Мінск"

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8481002138:AAHtukBqtj9B_IqsQDulkeKtiaSSKnOU8-c")
+CHANNEL_ID = os.getenv("CHANNEL_ID", "@mariushHP2009")
+
+OWM_API_KEY = os.getenv("OWM_API_KEY", "")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY", "")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+
+CITY = "Мінск"
